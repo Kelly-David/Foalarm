83d07789e983bbe9f4fd7b2f5d260937ab6e9383
kelly-david <davidkelly.tlf@gmail.com>
2017-12-17 21:26:56 +0000

    Upload image

--- application/foalarm/src/app/user-profile/user-profile.component.ts
+++ application/foalarm/src/app/user-profile/user-profile.component.ts
@@ -9,6 +9,8 @@ import { AlertHandlerService } from '../alert-handler.service';
 import { User } from '../user';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
 
 @Component({
   selector: 'app-user-proifle',
@@ -22,6 +24,8 @@ export class UserProfileComponent implements OnInit {
 
   public user = {} as User;
   public alertString: string;
+  public loading = false as Boolean;
+  public loaded = false as Boolean;
 
   constructor(
     public fb: FormBuilder,
@@ -44,7 +48,8 @@ export class UserProfileComponent implements OnInit {
       'password': ['', [
         Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
         Validators.minLength(6),
-        Validators.maxLength(25)
+        Validators.maxLength(25),
+        Validators.required
       ]
       ]
     });
@@ -53,14 +58,25 @@ export class UserProfileComponent implements OnInit {
     this.detailForm = this.fb.group({
       'foalarm': ['', [
         Validators.required
-      ]]
+      ]],
+      'fullName': ['', [
+        Validators.required
+      ]],
+      'location': ['', [
+        Validators.required
+      ]],
+      'photoURL': ['', []]
     });
   }
 
   // Getters reduce the amount of typescript code!
   get email() { return this.signupForm.get('email'); }
   get password() { return this.signupForm.get('password'); }
+
   get foalarm() { return this.detailForm.get('foalarm'); }
+  get fullName() { return this.detailForm.get('fullName'); }
+  get location() { return this.detailForm.get('location'); }
+  get photoURL() { return this.detailForm.get('photoURL'); }
 
   // Step 1
   signup() {
@@ -69,7 +85,25 @@ export class UserProfileComponent implements OnInit {
 
   // Step 2
   setFoalarm(user) {
-    return this.authService.updateUserData(user, { foalarm: this.foalarm.value });
+    return this.authService.updateUserData(user, {
+      fullName: this.fullName.value,
+      foalarm: this.foalarm.value,
+      location: this.location.value,
+      photoURL: this.user.photoURL
+    });
+  }
+
+  uploadFile(user: User, event: any) {
+    this.loading = true;
+    const file = event.srcElement.files[0];
+    const storageRef = firebase.storage().ref(`users/${user.uid}`);
+    console.log(user.uid);
+    storageRef.put(file)
+      .then(uploadTask => this.user.photoURL = uploadTask.downloadURL)
+      .then(_ => {
+        this.loaded = true;
+      });
+
   }
 
 }
