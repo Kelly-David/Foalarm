5659d7d223d155f4fd34b7fe4696fbbb70fb4bf8
kelly-david <davidkelly.tlf@gmail.com>
2017-12-29 14:35:16 +0000

    Updated AlarmEditComponent

--- application/foalarm/src/app/alarm/alarm-edit/alarm-edit.component.ts
+++ application/foalarm/src/app/alarm/alarm-edit/alarm-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Alarm } from '../../alarm';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { AlarmService } from '../alarm.service';
+import { AuthService } from '../../core/auth.service';
+import { AlertHandlerService } from '../../alert-handler.service';
 
 @Component({
   selector: 'app-alarm-edit',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmEditComponent implements OnInit {
 
-  constructor() { }
+  alarmForm: FormGroup;
+  public alarmObject = {} as Alarm;
+  isNewAlarm: boolean;
+  alarmKey: string;
+  alarm$: Observable<Alarm> | Observable<any>;
+  public alertString;
+  exists = false as boolean;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private alarmService: AlarmService,
+    private authService: AuthService,
+    public fb: FormBuilder,
+    private ahs: AlertHandlerService
+  ) { }
 
   ngOnInit() {
+    // Get the key from the URL
+    this.alarmKey = this.activatedRoute.snapshot.params['id'];
+    // Is it a new alarm?
+    this.isNewAlarm = this.alarmKey === 'new';
+    // Retrieve the alarm instance OR create a new object observable
+    !this.isNewAlarm ? this.getAlarm() : this.alarm$ = Observable.of({}) as Observable<Alarm>;
+    // Testing
+    console.log(this.alarmKey);
   }
 
+
+  private getAlarm(): Observable<Alarm> {
+    // TODO
+    return null;
+  }
 }
