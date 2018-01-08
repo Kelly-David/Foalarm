6cdfcefcdfb4c4084c0f9e362d5a9679ff4f4ccc
kelly-david <davidkelly.tlf@gmail.com>
2018-01-08 10:51:48 +0000

    Cloud Functions: TestFoalAert: add confition if phone exists

--- application/foalarm/functions/index.js
+++ application/foalarm/functions/index.js
@@ -16,35 +16,35 @@ const twilioPhoneNumber = '+353861801437';
 /// Start the Cloud Function
 
 exports.textFoalAlert = functions.firestore.document('data/{key}')
-.onUpdate(event => {
-    const alarmId = event.data.id;
-    const firestore = event.data.ref.firestore;
+    .onUpdate(event => {
+        const alarmId = event.data.id;
+        const firestore = event.data.ref.firestore;
 
-    return admin.firestore().doc(`alarms/${alarmId}`).get().then(function(doc) {
-        if (doc.exists) {
-            console.log('Document data: ', doc.data());
-            const phoneNumber = doc.data().phone;
-            console.log('Phone number: ', phoneNumber);
-            if(doc.data().alert === true) {
+        return admin.firestore().doc(`alarms/${alarmId}`).get().then(function (doc) {
+            if (doc.exists) {
+                console.log('Document data: ', doc.data());
+                const phoneNumber = doc.data().phone;
+                console.log('Phone number: ', phoneNumber);
+                if (doc.data().phone) {
 
-                const textMessage = {
-                    body: `Current alarm status`,
-                    to: phoneNumber,  // Text to this number
-                    from: twilioPhoneNumber // From a valid Twilio number
+                    const textMessage = {
+                        body: `Current alarm status`,
+                        to: phoneNumber,  // Text to this number
+                        from: twilioPhoneNumber // From a valid Twilio number
+                    }
+                    return client.messages.create(textMessage)
+                        .then(message => console.log(message.sid, 'Success'))
+                        .catch(error => console.log('Twilio send error: ', error));
                 }
-                return client.messages.create(textMessage)
-                .then(message => console.log(message.sid, 'Success'))
-                .catch(error => console.log('Twilio send error: ', error));
-            }
 
-        } else{
-            console.log('Not such document exists');
-        }
-    }).catch(function(error) {
-        console.log('Error caught: ', error);
+            } else {
+                console.log('Not such document exists');
+            }
+        }).catch(function (error) {
+            console.log('Error caught: ', error);
+        });
+
     });
-              
-});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
