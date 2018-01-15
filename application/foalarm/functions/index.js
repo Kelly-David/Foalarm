f41f5bf3b7ebb04639ffba3fb2465e259032491b
kelly-david <davidkelly.tlf@gmail.com>
2018-01-15 12:20:47 +0000

    Cloud Functions - saveFoalingalert

--- application/foalarm/functions/index.js
+++ application/foalarm/functions/index.js
@@ -135,3 +135,45 @@ exports.sendFoalAlertEmail = functions.firestore
                 }
             });
     });
+
+// Start Cloud Function
+exports.saveFoalingAlert = functions.firestore
+    .document('data/{key}/data/{dataKey}')
+    .onWrite(event => {
+        const alarmId = event.params.key;
+        return admin.firestore().doc(`alarms/${alarmId}`)
+            .get()
+            .then(function (doc) {
+                if (doc.exists) {
+                    const email = doc.data().emailAddress;
+                    if (email) {
+                        const alarmKey = doc.data().id;
+                        const email = doc.data().emailAddress;
+
+                        admin.firestore().collection('horses')
+                            .where('alarmId', '==', alarmKey)
+                            .get()
+                            .then(snapshotQuery => {
+                                snapshotQuery.forEach(doc => {
+                                    if (doc.data().alarmId == alarmKey) {
+                                        const horse = doc.data();
+                                        const msg = {
+                                            to: email,
+                                            from: 'alerts@foalarm.com',
+                                            subject: 'Foaling Alert!',
+                                            templateId: '957f3c38-c900-4ecb-8a02-8022509799a9',
+                                            substitutionWrappers: ['{{', '}}'],
+                                            substitutions: {
+                                                horseName: horse.displayName
+                                            }
+                                        };
+                                        return sgMail.send(msg)
+                                            .then(() => console.log('email sent!'))
+                                            .catch(err => console.log(err))
+                                    }
+                                });
+                            });
+                    }
+                }
+            });
+    });
