1709f0066bca0d6a880b4e94f2922bc4b88d4ead
kelly-david <davidkelly.tlf@gmail.com>
2018-01-18 11:22:09 +0000

    Alert num badge to nav button

--- application/foalarm/src/app/nav/nav-list/nav-list.component.ts
+++ application/foalarm/src/app/nav/nav-list/nav-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Alert } from '../../alert';
+import { AlertsService } from '../../alerts/alerts.service';
 
 @Component({
   selector: 'app-nav-list',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavListComponent implements OnInit {
 
-  constructor() { }
+  alerts$: Observable<Alert[]> | Observable<any> | null;
+
+  constructor(
+    private alertsService: AlertsService
+  ) { }
 
   ngOnInit() {
+
+    this.getAlerts();
   }
 
+  getAlerts() {
+    this.alerts$ = this.alertsService.alerts$;
+  }
 }
