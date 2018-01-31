a54230f654c80c80dbfb1a92e56b7c90ae69d6bb
kelly-david <davidkelly.tlf@gmail.com>
2018-01-31 12:38:06 +0000

    Foaling calculator

--- application/foalarm/src/app/tools/calc/calc.component.ts
+++ application/foalarm/src/app/tools/calc/calc.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcComponent implements OnInit {
 
+  collapse: Boolean = true;
+  service: any;
+  foalDate320: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleCollapse() {
+    this.collapse = !this.collapse;
+  }
+
+  getFoalingDates() {
+    this.foalDate320 = new Date(this.service);
+    this.foalDate320.setDate( this.foalDate320.getDate() + 320 );
+
+    // this.foalDate.day340 = this.foalDate.service + 340;
   }
 
 }
