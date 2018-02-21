d68525a2a646bbcea900e30110a792d9ff419cd2
kelly-david <davidkelly.tlf@gmail.com>
2018-02-21 11:48:24 +0000

    Added trigger to generate csv file

--- application/foalarm/src/app/data/generate-report/generate-report.component.ts
+++ application/foalarm/src/app/data/generate-report/generate-report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-generate-report',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateReportComponent implements OnInit {
 
-  constructor() { }
+  orders: Observable<any>;
+  reportsRef: AngularFirestoreCollection<any>;
+  reports: Observable<any>;
+
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
+
+    this.orders = this.afs.collection('orders').valueChanges();
+    this.reportsRef = this.afs.collection('reports');
+
+    // Map the snapshot to include the document ID
+    this.reports = this.reportsRef
+      .snapshotChanges().map(arr => {
+      return arr.map(snap => {
+        const data = snap.payload.doc.data();
+        const id = snap.payload.doc.id;
+        return { ...data, id };
+      });
+    });
+
+  }
+
+  // Creates new report, triggering FCF
+  requestReport() {
+    const data = {
+      status: 'processing',
+      createdAt: new Date()
+    };
+    this.reportsRef.add(data);
   }
 
 }
