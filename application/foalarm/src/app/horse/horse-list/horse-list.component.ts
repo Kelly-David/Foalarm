70b3130e65cce39b94da8d316ec1d69bc6cde5eb
kelly-david <davidkelly.tlf@gmail.com>
2018-03-27 16:43:48 +0100

    Horse Edit - import modal modules

--- application/foalarm/src/app/horse/horse-list/horse-list.component.ts
+++ application/foalarm/src/app/horse/horse-list/horse-list.component.ts
@@ -5,6 +5,9 @@ import { AlertHandlerService } from '../../alert-handler.service';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../core/auth.service';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+import { BsModalService } from 'ngx-bootstrap';
+import { BsModalRef } from 'ngx-bootstrap';
+import { AlarmEditModalComponent } from '../../alarm/alarm-edit-modal/alarm-edit-modal.component';
 
 @Component({
   selector: 'app-horse-list',
@@ -15,10 +18,12 @@ export class HorseListComponent implements OnInit {
 
   public horse = {} as Horse;
   public horses$: Observable<Horse[]> | Observable<any>;
+  bsModalRef: BsModalRef;
 
   constructor(
     public horseService: HorseService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private modalService: BsModalService
   ) {
   }
 
@@ -36,6 +41,12 @@ export class HorseListComponent implements OnInit {
   getStyle(imageUrl) {
     const style = `background-image: url(${imageUrl}) !important; background-size: cover`;
     return this.sanitizer.bypassSecurityTrustStyle(style);
+  }
+
+  // Create a model dialog - title is passed as input to the child component.
+  openClientModal(id?: string) {
+    this.bsModalRef = this.modalService.show(AlarmEditModalComponent, {class: 'modal-dialog'});
+    this.bsModalRef.content.key = id;
   }
 
 }
