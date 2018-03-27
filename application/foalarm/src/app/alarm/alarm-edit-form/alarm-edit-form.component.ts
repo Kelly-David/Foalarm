5943fe25ecc6ef53046f9af0a1fd21dd28927bd1
kelly-david <davidkelly.tlf@gmail.com>
2018-03-27 10:48:59 +0100

    Alarm edit form as modal child component

--- application/foalarm/src/app/alarm/alarm-edit-form/alarm-edit-form.component.ts
+++ application/foalarm/src/app/alarm/alarm-edit-form/alarm-edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alarm-edit-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alarm-edit-form.component.css']
 })
 export class AlarmEditFormComponent implements OnInit {
+
+  @Input() alarmId: any;
 
   constructor() { }
 
