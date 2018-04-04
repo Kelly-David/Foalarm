99f9e633d8230e4911af8c2bca852bf28211037c
kelly-david <davidkelly.tlf@gmail.com>
2018-04-04 13:27:25 +0100

    Refactored user to public-user comp

--- application/foalarm/src/app/user-profile/public-user/public-user.component.ts
+++ application/foalarm/src/app/user-profile/public-user/public-user.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-public-user',
   templateUrl: './public-user.component.html',
   styleUrls: ['./public-user.component.css']
 })
-export class PublicUserComponent implements OnInit {
+export class PublicUserComponent implements OnChanges {
 
-  constructor() { }
+  @Input() uid: any; // The UID of the user
+  @Input() user: any; // The UID of auth user
+  user$: Observable<{}> | Observable<any>;
 
-  ngOnInit() {
+  constructor(
+    private userService: UserService
+  ) { }
+
+  ngOnChanges() {
+    this.user$ = this.userService.getUser(this.uid);
+  }
+
+  addFriend(uid: string, fullName: string) {
+    console.log('Added friend', uid, ' By', this.user);
+    return this.userService.addFriend(this.user, {uid: uid, fullName: fullName}, uid);
   }
 
 }
