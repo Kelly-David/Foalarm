1c11abdcd8ba983a63fab5ae58dc831b5fbe0593
kelly-david <davidkelly.tlf@gmail.com>
2018-04-12 15:42:32 +0100

    Report link

--- application/foalarm/src/app/data/report-link/report-link.component.ts
+++ application/foalarm/src/app/data/report-link/report-link.component.ts
@@ -1,3 +1,15 @@
+/*
+ * File: report-link.component.ts
+ * Project: /Users/david/Foalarm/application/foalarm
+ * File Created: Wednesday, 21st February 2018 11:48:43 am
+ * Author: david
+ * -----
+ * Last Modified: Thursday, 12th April 2018 3:41:05 pm
+ * Modified By: david
+ * -----
+ * Description: Displays the download URL of an activity report
+ */
+
 import { Component, OnInit, Input } from '@angular/core';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
@@ -11,8 +23,7 @@ import { ReportService } from '../report.service';
 export class ReportLinkComponent implements OnInit {
 
   @Input() report: any;
-  downloadUrl: Observable<string>;
-
+  public downloadUrl: Observable<string>;
 
   constructor(private reportService: ReportService) { }
 
@@ -22,6 +33,10 @@ export class ReportLinkComponent implements OnInit {
     this.downloadUrl = Observable.fromPromise(promise);
   }
 
+  /**
+   * Remove the report (update deleted = true)
+   * @param key this.report
+   */
   remove(key: string) {
     this.reportService.removeReport(key);
   }
