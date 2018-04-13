fc9121031ba51f855f028e88b86db3d3dda2f43d
kelly-david <davidkelly.tlf@gmail.com>
2018-04-13 09:01:11 +0100

    Messaging service docs

--- application/foalarm/src/app/horse.ts
+++ application/foalarm/src/app/horse.ts
@@ -1,13 +1,13 @@
 export interface Horse {
-    id?: string;
-    photoURL?: string;
-    displayName?: string;
-    dueDate?: string;
-    camera?: string;
-    owner?: string;
-    state?: Boolean;
-    alert?: Boolean;
-    alarmId?: string;
-    ownerUID?: string;
-    isPublic?: boolean;
+    readonly    id?: string;
+                photoURL?: string;
+                displayName?: string;
+                dueDate?: string;
+                camera?: string;
+                owner?: string;
+                state?: Boolean;
+                alert?: Boolean;
+                alarmId?: string;
+                ownerUID?: string;
+                isPublic?: boolean;
 }
--- application/foalarm/src/app/messaging.service.ts
+++ application/foalarm/src/app/messaging.service.ts
@@ -1,3 +1,15 @@
+/*
+ * File: messaging.service.ts
+ * Project: /Users/david/Foalarm/application/foalarm
+ * File Created: Wednesday, 24th January 2018 4:26:16 pm
+ * Author: david
+ * -----
+ * Last Modified: Friday, 13th April 2018 9:00:39 am
+ * Modified By: david
+ * -----
+ * Description:
+ */
+
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -9,25 +21,31 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class MessagingService {
 
-  messaging = firebase.messaging();
-
-  currentMessage = new BehaviorSubject(null);
+  public messaging = firebase.messaging();
+  public currentMessage = new BehaviorSubject(null);
 
   constructor(
     private db: AngularFireDatabase,
     private auth: AngularFireAuth
   ) { }
 
+  /**
+   * Updates the tokoen ref in Firebase
+   * Get the current auth user UID, then update the ref.
+   * @param token FCM token
+   */
   private updateToken(token) {
     this.auth.authState.take(1).subscribe(user => {
       if (!user) { return; }
-
       const data = { [user.uid]: token };
       this.db.object('fcmTokens/').update(data);
     });
   }
 
-  getPermission() {
+  /**
+   * Get user permission, then save the token in Firebase RTDB
+   */
+  public getPermission() {
     this.messaging.requestPermission()
     .then(() => {
       console.log('Notifcation permission granted');
@@ -42,10 +60,13 @@ export class MessagingService {
     });
   }
 
-  receiveMessage() {
+  /**
+   * Receive a message and pass it to the subject
+   */
+  public receiveMessage() {
     this.messaging.onMessage((payload) => {
       console.log('Message received:, ', payload);
-      this.currentMessage.next(payload); // Pass the behaviour subject the next value
+      this.currentMessage.next(payload);
     });
   }
 }
